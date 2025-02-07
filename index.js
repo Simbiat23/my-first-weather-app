705bcbd872e4b12563d336954eb162084340a928
Simbiat23 <155582634+Simbiat23@users.noreply.github.com>
2025-02-07 18:55:56 +0000

    adding API key

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
+function changeWeatherInfo(response) {
+  let temperatureElement = document.querySelector("#current-temp-value");
+  let cityElement = document.querySelector("#city-input");
+
+  cityElement.innerHTML = response.data.city;
+
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+}
+
+function searchCity(city) {
+  let apiKey = `t07aaefccae3394of62526e7dc0c0bad`;
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(changeWeatherInfo);
+}
+
+// fuction to change city using user input
 function submitForm(event) {
   event.preventDefault();
   let searchFormInput = document.querySelector("#search-input");
-  let city = document.querySelector("#city-input");
-  city.innerHTML = searchFormInput.value;
+
+  searchCity(searchFormInput.value);
 }
 
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", submitForm);
+cityElement = response.data.city;
