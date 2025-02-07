843bddbd9f387eaf9119c0ce9492aab422d59ad5
Simbiat23 <155582634+Simbiat23@users.noreply.github.com>
2025-02-07 20:23:27 +0000

    added weather parameters to Js

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 function changeWeatherInfo(response) {
   let temperatureElement = document.querySelector("#current-temp-value");
   temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+
   let cityElement = document.querySelector("#city-input");
   cityElement.innerHTML = response.data.city;
 
   let tempDescription = document.querySelector("#weather-description");
   tempDescription.innerHTML = response.data.condition.description;
+
+  let humidity = document.querySelector("#humidity-value");
+  humidity.innerHTML = `${response.data.temperature.humidity}%`;
+
+  let wind = document.querySelector("#wind-value");
+  wind.innerHTML = `${response.data.wind.speed}km/h`;
+
+  let currentDate = document.querySelector("#current-date");
 }
 
 function searchCity(city) {
